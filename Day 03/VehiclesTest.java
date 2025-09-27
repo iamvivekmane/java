8aecae73686feeb8d94c9073bab8417f9289e51c
Vivek Mane <vivekmane172@gmail.com>
2025-09-27 09:32:37 +0530

    Vehicle class

--- Day 03/VehiclesTest.java
+++ Day 03/VehiclesTest.java
@@ -3,13 +3,12 @@ public class VehiclesTest {
         Car c1 = new Car();
         c1.drive();
         c1.stop();
-
         System.out.println("--------------");
         HoverCraft h1 = new HoverCraft();
         h1.launch();
         h1.drive();
         h1.enterLand();
-        h1.enterSea(); 
+        h1.enterSea();
         System.out.println("--------------");
         Ship s1 = new Ship();
         s1.launch();
