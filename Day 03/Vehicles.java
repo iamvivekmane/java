5107347797685e2c4505dd29a31dfd22f5ac7aac
Vivek Mane <vivekmane172@gmail.com>
2025-09-27 08:13:44 +0530

    Vehicle class

--- Day 03/Vehicles.java
+++ Day 03/Vehicles.java
@@ -64,21 +64,3 @@ class Ship implements SeaVehicle{
         System.out.println("The ship is turning");
     }
 }
-public class Vehicles {
-    public static void main(String[] args) {
-        Car c1 = new Car();
-        c1.drive();
-        c1.stop();
-
-        System.out.println("--------------");
-        HoverCraft h1 = new HoverCraft();
-        h1.launch();
-        h1.drive();
-        h1.enterLand();
-        h1.enterSea(); 
-        System.out.println("--------------");
-        Ship s1 = new Ship();
-        s1.launch();
-        s1.turn();
-    }
-}
---
+++ Day 03/VehiclesTest.java
@@ -0,0 +1,18 @@
+public class VehiclesTest {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.drive();
+        c1.stop();
+
+        System.out.println("--------------");
+        HoverCraft h1 = new HoverCraft();
+        h1.launch();
+        h1.drive();
+        h1.enterLand();
+        h1.enterSea(); 
+        System.out.println("--------------");
+        Ship s1 = new Ship();
+        s1.launch();
+        s1.turn();
+    }
+}
